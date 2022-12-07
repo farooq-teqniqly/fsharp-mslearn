e8135e7fd08ae0378692e690e21073c29e5757f5
Farooq <farooq@teqniqly.com>
2022-12-07 08:11:35 -0800

    Functions example

---
+++ first-steps/Calculator/Program.fs
@@ -0,0 +1,13 @@
+﻿let double (a: int) : int = a * 2
+let half (a: int) : int = a / 2
+
+let sumThenHalf = double >> half
+
+printfn "%d" (sumThenHalf 2)
+
+
+let list = [7; 11; 3]
+let sort (list: int list) = List.sort list
+let print (list: int list) = List.iter(fun x -> printfn "%i" x) list
+
+list |> sort |> print
