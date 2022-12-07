7ab65d1b16a2a782cc60f2ecd1e3ec243e2b1ae5
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2022-12-07 13:30:03 -0800

    List operations example

--- first-steps/Cards/Program.fs
+++ first-steps/Cards/Program.fs
@@ -1,5 +1,5 @@
 ﻿open System
-let cards = [21; 3; 1; 7; 9; 23]
+
 let cardFace (card: int) : string = 
     let cardNo: int = card % 13
 
@@ -8,6 +8,12 @@ let cardFace (card: int) : string =
     elif cardNo = 12 then "Queen"
     elif cardNo = 0 then "King"
     else string cardNo
+    
+let cardValue card =
+    let value = card % 13
+    if value = 0 then 11
+    elif value = 10 || value = 11 || value = 12 then 10
+    else value
 
 let suit (card: int) : string = 
     let suitNo: int = card / 13
@@ -27,5 +33,11 @@ let take (no: int) (list) = List.take no list
 
 [<EntryPoint>]
 let main argv =
-    cards |> shuffle |> take 3 |> printAll
+    let cards = [21; 3; 1; 7; 9; 23]
+    let hand = [0; 25; 32]
+    let sum = List.sumBy(fun card -> cardValue card) hand
+    printfn $"%i{sum}"
+    
+    
+    // cards |> shuffle |> take 3 |> printAll
     0
