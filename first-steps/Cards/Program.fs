81bc12602f0fa989db97c7291c5dd51cdcca4225
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2022-12-07 12:14:29 -0800

    Functions example.

--- first-steps/Cards/Program.fs
+++ first-steps/Cards/Program.fs
@@ -1,4 +1,6 @@
-﻿let cardDescription (card: int) : string = 
+﻿open System
+let cards = [21; 3; 1; 7; 9; 23]
+let cardFace (card: int) : string = 
     let cardNo: int = card % 13
 
     if cardNo = 1 then "Ace"
@@ -7,19 +9,23 @@
     elif cardNo = 0 then "King"
     else string cardNo
 
-let suit (no: int) : string = 
-    let suitNo: int = no / 13
+let suit (card: int) : string = 
+    let suitNo: int = card / 13
 
     if suitNo = 0 then "Hearts"
     elif suitNo = 1 then "Spades"
     elif suitNo = 2 then "Diamonds"
     else "Clubs"
+    
+let shuffle list =
+    let random = Random()
+    list |> List.sortBy(fun _ -> random.Next())
+    
+let printCard card =  printfn $"%s{cardFace(card)} of %s{suit(card)}"
+let printAll list = List.iter(printCard) list
+let take (no: int) (list) = List.take no list
 
 [<EntryPoint>]
-let main argv = 
-    let cards = [1; 10; 2; 34]
-
-    for card in cards do
-        printfn "%s of %s" (cardDescription(card)) (suit(card))
-    
+let main argv =
+    cards |> shuffle |> take 3 |> printAll
     0
