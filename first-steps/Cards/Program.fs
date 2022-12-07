ff1b7d5063c8e0445af3d81a6a4deb9e0348711f
Farooq Mahmud <49829654+farooq-teqniqly@users.noreply.github.com>
2022-12-07 13:31:47 -0800

    F# tutorial code

--- first-steps/Cards/Program.fs
+++ first-steps/Cards/Program.fs
@@ -1,4 +1,6 @@
-﻿let cardDescription (card: int) : string = 
+﻿open System
+
+let cardFace (card: int) : string = 
     let cardNo: int = card % 13
 
     if cardNo = 1 then "Ace"
@@ -6,20 +8,36 @@
     elif cardNo = 12 then "Queen"
     elif cardNo = 0 then "King"
     else string cardNo
+    
+let cardValue card =
+    let value = card % 13
+    if value = 0 then 11
+    elif value = 10 || value = 11 || value = 12 then 10
+    else value
 
-let suit (no: int) : string = 
-    let suitNo: int = no / 13
+let suit (card: int) : string = 
+    let suitNo: int = card / 13
 
     if suitNo = 0 then "Hearts"
     elif suitNo = 1 then "Spades"
     elif suitNo = 2 then "Diamonds"
     else "Clubs"
+    
+let shuffle list =
+    let random = Random()
+    list |> List.sortBy(fun _ -> random.Next())
+    
+let printCard card =  printfn $"%s{cardFace(card)} of %s{suit(card)}"
+let printAll list = List.iter(printCard) list
+let take (no: int) (list) = List.take no list
 
 [<EntryPoint>]
-let main argv = 
-    let cards = [1; 10; 2; 34]
-
-    for card in cards do
-        printfn "%s of %s" (cardDescription(card)) (suit(card))
+let main argv =
+    let cards = [21; 3; 1; 7; 9; 23]
+    let hand = [0; 25; 32]
+    let sum = List.sumBy(fun card -> cardValue card) hand
+    printfn $"%i{sum}"
     
+    
+    // cards |> shuffle |> take 3 |> printAll
     0
---
+++ first-steps/scratch.fsx
@@ -0,0 +1,79 @@
+﻿open System
+
+let drawCard (tuple: int list * int list) =
+    let deck = fst tuple
+    let draw = snd tuple
+    let firstCard = deck.Head
+    
+    printfn "%i" firstCard
+    
+    let hand = draw |> List.append [firstCard]
+    
+    (deck.Tail, hand)
+
+let cards = [0..5]
+let hand = []
+
+
+let d, h = (cards, hand) |> drawCard |> drawCard |> drawCard
+
+printfn $"Deck: %A{d} Hand: %A{h}"
+
+List.iter(fun i -> printfn $"%i{i}") cards
+
+let cards = [0..5]
+let evens = List.filter(fun i -> i % 2 = 0) cards
+
+let found = List.tryFind(fun i -> i = 2) evens
+
+printfn $"%A{evens}"
+
+open System
+let cards = [0..10]
+let random = Random()
+List.sortBy(fun _ -> random.Next()) cards |> List.sortDescending |> List.iter(fun i -> printfn $"%i{i}")
+
+printfn $"%A{cards}"
+
+type Person = {FirstName: string; LastName: string}
+let people: Person list = [
+    {FirstName = "Albert"; LastName = "Einstein"}
+    {FirstName = "Marie"; LastName = "Curie"}
+]
+
+
+type Weather = {City: string; Temp: int}
+
+let weatherReport: Weather list = [
+    {City = "Seattle"; Temp = 43}
+    {City = "Portland"; Temp = 47}
+    {City = "Miami"; Temp = 75}
+    {City = "Honolulu"; Temp = 80}
+]
+
+let sum = List.sumBy(fun (w: Weather) -> w.Temp) weatherReport
+let avg = List.averageBy(fun (w: Weather) -> double w.Temp) weatherReport
+
+List.sortByDescending(fun (w: Weather) -> w.Temp) weatherReport
+    |> List.iter(fun (w: Weather) -> printfn $"City: %s{w.City} Temp: %i{w.Temp}")
+let nobelPrizeWinners = List.map(fun p -> p.FirstName + " " + p.LastName) people
+printfn $"%A{nobelPrizeWinners}"
+
+type MagicCreature = {Name: string; Level: int; Attack: int;}
+let creatures = [
+    {Name = "Dragon"; Level = 2; Attack = 20;}
+    {Name = "Orc"; Level = 1; Attack = 5;}
+    {Name = "Demon"; Level = 2; Attack = 10;}
+]
+
+let creatureComparer (c1: MagicCreature) (c2: MagicCreature) =
+    if c1.Level < c2.Level then -1
+    else if c1.Level > c2.Level then 1
+    else if c1.Attack < c2.Attack then -1
+    else if c1.Attack > c2.Attack then 1
+    else 0
+
+let sorted = List.sortWith creatureComparer creatures
+
+let l1 = [0..100]
+let sum = List.sum l1
